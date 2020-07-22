c39a9f32edb67a5c16d57c7c7aa2843bb75ed227
Camila Roque <59898248+milaroque@users.noreply.github.com>
2020-07-22 20:23:41 -0300

    Update register.js

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -92,12 +92,12 @@ return (
         <Input type="password" id="passwordConfirm" class="input-text" value={passwordConfirm} onChange={e => setPasswordConfirm(e.target.value)} placeholder="Confirme a sua senha" />
         <Button id="register" class="btn-register" type='submit' onClick={createUser} name="Registrar" />
         <div className='select-type'>
-          <Image src={Chef} alt='img-chef' class='chef-kitchen'/>
+          <Image src={chef} alt='img-chef' class='chef-kitchen'/>
           <label htmlFor='cozinha' className='label-kitchen'>COZINHA</label>
           <Input type='radio' checked class='btn-radio-kitchen' name='job' value='kitchen' onChange={e => setJob(e.target.value)} id='kitchen' />
           </div>
           <div className='select-type-two'>
-            <Image src={Hall} alt='img-hall' class='img-hall'/>
+            <Image src={hall} alt='img-hall' class='img-hall'/>
             <label htmlFor='salao' className='label-hall'>SALÃO</label>
             <Input type='radio' class='btn-radio-hall' name='job' value='hall' onChange={e => setJob(e.target.value)} id='hall' />
           </div>
