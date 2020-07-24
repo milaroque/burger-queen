4dd8defcb94f8384b10a5f46fa0395c1d6d5caac
Camila Roque <milaroque@icloud.com>
2020-07-23 21:00:24 -0300

    olho senha

---
+++ build/precache-manifest.1e40c8d8f4eabbf9d2a97525839f71e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df79690bf9822d5e6cfca496c309bcb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "107585b806a21784ecaa",
+    "url": "/static/css/main.8c11881d.chunk.css"
+  },
+  {
+    "revision": "ae5e8f7ac53a5e96d62f",
+    "url": "/static/js/2.ded3606e.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.ded3606e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107585b806a21784ecaa",
+    "url": "/static/js/main.93b5e5df.chunk.js"
+  },
+  {
+    "revision": "ed2d4e4548a84020adda",
+    "url": "/static/js/runtime-main.f649d344.js"
+  },
+  {
+    "revision": "daaa92cebb374f9c2de192ea0e3f9da0",
+    "url": "/static/media/chef.daaa92ce.png"
+  },
+  {
+    "revision": "49b4572c4a23befc3520534b76b37801",
+    "url": "/static/media/hall.49b4572c.png"
+  },
+  {
+    "revision": "424fb1ed551c7b1f5ea6840b11b54b38",
+    "url": "/static/media/logo.424fb1ed.png"
+  }
+]);
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,22 +1,31 @@
-import React, {useState} from 'react';
-import Button from '../../components/button/Button'
-import Input from '../../components/input/Input'
-import Image from '../../components/image/image'
-import logo from '../../assets/logo.png'
-import Modal from '../../components/modal/modal';
-import Register from '../register/register';
+import React, { useState } from "react";
+import Button from "../../components/button/Button";
+import Input from "../../components/input/Input";
+import Image from "../../components/image/image";
+import logo from "../../assets/logo.png";
+import Modal from "../../components/modal/modal";
+import Register from "../register/register";
 import firebase from "../../config/firebase.js";
 import "firebase/firebase-auth";
 import "firebase/firebase-firestore";
 import Swal from "sweetalert2";
 import { useHistory } from "react-router-dom";
-import './login.css'
+import "./login.css";
+import authErrors from "../../config/errorsfirebase";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 const Login = () => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [email, setEmail] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordShown, setPasswordShown] = useState(false);
   const history = useHistory();
+  const eyes = <FontAwesomeIcon icon={faEye} />;
+
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
 
   const loginUser = () => {
     if (!email) {
@@ -39,23 +48,18 @@ const Login = () => {
             .collection("users")
             .doc(uid.user.uid)
             .get()
-            .then((doc) => { 
+            .then((doc) => {
               if (doc.data().job === "hall") {
                 history.push("/newRequest");
               } else {
                 history.push("/ordersReceived");
-              }  
-            })
-        }) 
-        .catch(function (error) {
-          if (error.code === "auth/user-not-found") {
-            Swal.fire({
-              text: "Usuário não encontrado",
-              icon: "warning",
+              }
             });
-          } else if (error.code === "auth/wrong-password") {
+        })
+        .catch(function (error) {
+          if (authErrors[error.code]) {
             Swal.fire({
-              text: "Senha inválida",
+              text: authErrors[error.code],
               icon: "warning",
             });
           } else {
@@ -73,26 +77,46 @@ const Login = () => {
     loginUser(email, password);
   };
 
-return (
-    <div className='div'>
-        <figure>
-        <Image src={logo} alt='logo' class='logo-login'/>
-        </figure>
-        <div className='select-form'>
-            <form className= 'form-login'>
-                <Input type="email" id="email" class='input-login'  value={email}
-          onChange={(e) => setEmail(e.target.value)} placeholder="Digite seu email" />
-                <Input type="password" id="password" class='input-login' value={password}
-          onChange={(e) => setPassword(e.target.value)} placeholder="Digite sua senha" />
-                <Button id="login" class="button-loggin" name="Entrar" onClick={signIn}/>
-                <p onClick={ () => setIsModalVisible(true) }>Ainda não tem cadastro? Registre-se aqui!</p>
-                {isModalVisible ? (
-                <Modal onClose={ () => setIsModalVisible(false) }>
-                    <Register />
-                </Modal>
-                ) : null}
-            </form>
-        </div>
+  return (
+    <div className="div">
+      <figure>
+        <Image src={logo} alt="logo" class="logo-login" />
+      </figure>
+      <div className="select-form">
+        <form className="form-login">
+          <Input
+            type="email"
+            id="email"
+            class="input-login"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Digite seu email"
+          />
+          <Input
+            type={passwordShown ? "text" : "password"}
+            id="password"
+            class="input-login"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Digite sua senha"
+          />
+          <i className="eye" onClick={togglePasswordVisiblity}>{eyes}</i>
+          <Button
+            id="login"
+            class="button-loggin"
+            name="Entrar"
+            onClick={signIn}
+          />
+          <p onClick={() => setIsModalVisible(true)}>
+            Ainda não tem cadastro? Registre-se aqui!
+          </p>
+          {isModalVisible ? (
+            <Modal onClose={() => setIsModalVisible(false)}>
+              <Register />
+            </Modal>
+          ) : null}
+        </form>
+      </div>
     </div>
   );
 };
