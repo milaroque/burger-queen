30eb5013c59a99a44c6d863baea5bdaead0ccfa7
Camila Roque <milaroque@icloud.com>
2020-07-28 14:23:03 -0300

    imagem

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Button from "../button/Button";
 
-const Menu = () => {
-    
-    }
+const Menu = (props) => {
+    return ( <Button>
+      <div> { props.name } </div> 
+      <div> { props.price } </div> 
+      </Button>
+    );
+  }
 
 
 export default Menu;
