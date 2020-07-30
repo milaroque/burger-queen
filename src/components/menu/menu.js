87e84f2713f06d2a8de4b1ee172b3406ae379c13
alessandramarinho <allemmarinho@gmail.com>
2020-07-30 18:21:01 -0300

    pra ver se deu certo

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,16 +1,18 @@
 import React from "react";
 import './menu.css'
+
 function Menu(props) {
-  return (<button className={props.class}
-        onClick={props.onClick}
-        key={props.value}>
-        <div>
+  return (
+    <button className={props.class}
+      onClick={props.onClick}
+      key={props.value}>
+      <div>
         <img src={props.src} alt={props.alt} className='imagem'></img>
-        </div>
-        <div className={props.className}> {props.name} </div> 
-        <div className={ props.className }> {props.price} </div> 
-        </button>
-    ) 
+      </div>
+      <div className="name"> {props.name} </div>
+      <div className="price"> {props.price} </div>
+    </button>
+  )
 }
 
 export default Menu;
--- src/pages/hall/newRequest.js
+++ src/pages/hall/newRequest.js
@@ -3,15 +3,15 @@ import React, { useEffect, useState } from 'react'
 import Menu from '../../components/menu/menu';
 import getMenu from '../../functions/firebaseRead'
 import Nav from "../../components/nav/Nav";
-
+import "./newRequest.css"
 const NewRequest = () => {
-const [breakfast, setBreakfast] = useState([])
-const [hamburger, setHamburger] = useState([])
-const [sideDishes, setSideDishes] = useState([])
-const [drinks, setDrinks] = useState([])
+  const [breakfast, setBreakfast] = useState([])
+  const [hamburger, setHamburger] = useState([])
+  const [sideDishes, setSideDishes] = useState([])
+  const [drinks, setDrinks] = useState([])
 
 
-useEffect(() => {
+  useEffect(() => {
     getMenu().then(([breakfast, hamburger, sideDishes, drinks]) => {
 
       setBreakfast(breakfast)
@@ -22,71 +22,71 @@ useEffect(() => {
 
   }, []) //eslint-disable-line
 
-      return (
+  return (
     <main>
-        <div link='/newRequest'>
-       <Nav>
-         
-       </Nav>
+      <div link='/newRequest'>
+        <Nav>
+
+        </Nav>
       </div>
-    <section>
-      <div>MENU</div>
-      <div>Café da Manhã</div> {
-                (
-                  breakfast.map(item => (
-                    <Menu name={`${item.item} `}
-                    price={`R$${item.price}`}
-                    value={item.id}
-                    key={item.id}
-                    src={item.icon}
-                    className='image'
-                  />
-                  ))
-                ) 
-            } </section>
-            <section>
-      <div>Hamburgers</div> {
-                (
-                  hamburger.map(item => (
-                    <Menu name={`${item.item} `}
-                    price={`R$${item.price}`}
-                    value={item.id}
-                    key={item.id}
-                    src={item.icon}
-                  />
-                  ))
-                ) 
-            } </section>
-            <section>
-      <div>Acompanhamentos</div> {
-                (
-                  sideDishes.map(item => (
-                    <Menu name={`${item.item} `}
-                    price={`R$${item.price}`}
-                    value={item.id}
-                    key={item.id}
-                    src={item.icon}
-                  />
-                  ))
-                ) 
-            } </section>
-            <section>
-      <div>Bebidas</div> {
-                (
-                  drinks.map(item => (
-                    <Menu name={`${item.item} `}
-                    price={`R$${item.price}`}
-                    value={item.id}
-                    key={item.id}
-                    src={item.icon}
-                  />
-                  ))
-                ) 
-            } </section>
-            
-      </main>
-  
-    ) 
+      <section className="menu">
+        <div className="header-hall">Novo Pedido</div>
+        <div className="header-hall">Café da Manhã</div> {
+          (
+            breakfast.map(item => (
+              <Menu name={`${item.item} `}
+                price={`R$${item.price}`}
+                value={item.id}
+                key={item.id}
+                src={item.icon}
+                className='image'
+              />
+            ))
+          )
+        } </section>
+      <section>
+        <div>Hamburgers</div> {
+          (
+            hamburger.map(item => (
+              <Menu name={`${item.item} `}
+                price={`R$${item.price}`}
+                value={item.id}
+                key={item.id}
+                src={item.icon}
+              />
+            ))
+          )
+        } </section>
+      <section>
+        <div>Acompanhamentos</div> {
+          (
+            sideDishes.map(item => (
+              <Menu name={`${item.item} `}
+                price={`R$${item.price}`}
+                value={item.id}
+                key={item.id}
+                src={item.icon}
+              />
+            ))
+          )
+        } </section>
+      <section>
+        <div>Bebidas</div> {
+          (
+            drinks.map(item => (
+              <Menu name={`${item.item} `}
+                price={`R$${item.price}`}
+                value={item.id}
+                key={item.id}
+                src={item.icon}
+              />
+            ))
+          )
+        } </section>
+
+    </main>
+
+  )
 
 }
 
