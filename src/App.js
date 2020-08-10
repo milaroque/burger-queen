0d95716343035701e3e8604773be15c485cc1ed8
alessandramarinho <allemmarinho@gmail.com>
2020-08-10 15:16:59 -0300

    Rotas privadas

--- src/App.js
+++ src/App.js
@@ -11,18 +11,28 @@ import {
 import OrdersDelivery from './pages/hall/ordersToBeDelivered';
 import HistoricOrders from './pages/hall/historicOrders';
 import HistoricOrdersKitchen from './pages/kitchen/historicOrdersKitchen';
+import firebase from './config/firebase';
+import 'firebase/firebase-auth'
 
 export default function App() {
-
+  const PrivateRoute = ({component: Component, ...rest}) => {
+    return (
+        <Route {...rest} render={props => (
+            firebase.auth().currentUser ?
+                <Component {...props} />
+            : <Redirect to="/login" />
+        )} />
+    );
+    };
   return (
     <Router>
         <Switch>
           <Route path="/login" component={Login}/>
-          <Route path="/ordersReceived" component={OrdersReceived}/>
-          <Route path="/newRequest" component={NewRequest}/>
-          <Route path="/ordersToBeDelivered" component={OrdersDelivery}/>
-          <Route path="/historicOrders" component={HistoricOrders}/>
-          <Route path="/historicOrdersKitchen" component={HistoricOrdersKitchen}/>
+          <PrivateRoute path="/ordersReceived" component={OrdersReceived}/>
+          <PrivateRoute path="/newRequest" component={NewRequest}/>
+          <PrivateRoute path="/ordersToBeDelivered" component={OrdersDelivery}/>
+          <PrivateRoute path="/historicOrders" component={HistoricOrders}/>
+          <PrivateRoute path="/historicOrdersKitchen" component={HistoricOrdersKitchen}/>
           <Route exact path="/" component={Login}/>
         </Switch>
     </Router>
